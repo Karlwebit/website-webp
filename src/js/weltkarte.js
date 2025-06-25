b65e2d6d7f0c2f1e13cadf1cd462e4158fa0e08b
Karlwebit <karpo2005@gmx.de>
2025-06-25 15:54:08 +0200

    [FEATURE] gallery and recipes, pingPong are finished

--- src/js/weltkarte.js
+++ src/js/weltkarte.js
@@ -1,8 +1,16 @@
 import L from "leaflet";
 
-// Initialisiere die Karte
-const map = L.map("map").setView([20, 0], 2);
+// Karte initialisieren
+const map = L.map("map").setView([20, 0], 2); // Breite, Länge, Zoom
 
+// TileLayer (OpenStreetMap) hinzufügen
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution: "&copy; OpenStreetMap contributors",
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
+
+// Beispiel-Marker
+L.marker([51.505, -0.09])
+  .addTo(map)
+  .bindPopup("Hallo von Leaflet!")
+  .openPopup();
