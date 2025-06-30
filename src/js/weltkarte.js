b1f3239d9ff82b7d993b67ee587fae0ba9667f06
Karl <karpo2005@gmx.de>
2025-06-30 19:18:41 +0200

    [FEATURE] project finished

--- src/js/weltkarte.js
+++ src/js/weltkarte.js
@@ -2,18 +2,10 @@
 import "leaflet/dist/leaflet.css";
 // Importiert die Leaflet-Bibliothek
 import L from "leaflet";
-
-<<<<<<< HEAD
 // Initialisiert die Karte
 const map = L.map("map").setView([20, 0], 2);
 
-// Fügt die OpenStreetMap-Kachel-Layer hinzu
-=======
-// Karte initialisieren
-const map = L.map("map").setView([20, 0], 2); // Breite, Länge, Zoom
-
 // TileLayer (OpenStreetMap) hinzufügen
->>>>>>> b72afb9ecb33356ed7020a201fa7a7bb30ab59f2
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution:
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
